1c98c7f043f19f872eaa72c762846f85a046a080
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-13 16:04:53 -0400

    routing changed and server file adjusted

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,26 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-    // find all categories
+  // find all categories
   Category.findAll()
-  // Associated Products data
-  .then(dbProductData => res.json(dbProductData))
+  // console.log('category route for product data')
+    // Associated Products data
+    .then(dbProductData => res.json(dbProductData))
   console.log("ProductData ->", dbProductData)
-  .catch(Error => {
-    console.error(Error);
-  });
+    .catch(Error => {
+      console.error(Error);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   Category.findOne(req.params.id)
   console.log(req.params.id)
-  // Associated Products data
-  .then(dbProductData => res.json(dbProductData))
-  .catch(Error => {
-    console.error(Error);
-  });
+    // Associated Products data
+    .then(dbProductData => res.json(dbProductData))
+    .catch(Error => {
+      console.error(Error);
+    });
 });
 
 router.post('/', (req, res) => {
@@ -40,18 +41,33 @@ router.put('/:id', (req, res) => {
       id: res.params.id
     }
   })
-  .then(dbCategoryData => {
-    if (!dbCategoryData) {
-      res.status(404).json({ message: "Category Data did not align properly" })
-    }
-  })
-  .catch(Error => {
-    res.status(500).json
-  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "Category Data did not align properly" })
+      }
+    })
+    .catch(Error => {
+      res.status(500).json
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404)
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(Error => {
+      console.error(Error);
+    });
 });
 
 module.exports = router;
