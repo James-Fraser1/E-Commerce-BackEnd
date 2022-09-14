bf3f08b514e3dcac13c0164b08af432753b1e56d
James-Fraser1 <jdfraser2022@gmail.com>
2022-09-14 19:51:05 -0400

    GET and POST routes for tag-routes tested

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,39 +1,43 @@
 const router = require('express').Router();
-// const { RESERVED } = require('mysql2/lib/constants/client');
+const { RESERVED } = require('mysql2/lib/constants/client');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
-  Tag.findAll()
   // be sure to include its associated Product data
-  console.log('TAG ROUTE')
-    .then(TagData => {
-      res.json(TagData)
-    })
-  console.log("TagData ->", TagData)
+    Tag.findAll()
+      .then((tags) => 
+      res.status(200).json(tags))
+      .catch((err) => 
+      res.status(500).json(err))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   Tag.findOne()
-  console.log('Find one tag')
-  .then(TagData => {
-    res.json(TagData)
-  })
-  .catch(Error => {
-    console.error(Error);
-  });
+  .then((tags) => 
+  res.status(200).json(tags))
+  .catch((err) => 
+  res.status(500).json(err))
   // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then((tags) => 
+  res.status(200).json(tags))
+  .catch((err) => 
+  res.status(500).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
@@ -41,3 +45,5 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+
